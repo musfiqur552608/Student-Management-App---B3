5ec894f969b47bcf81f35990daee26fc59671c5f
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2024-05-21 11:06:28 +0600

    Viewmodel classes added

---
+++ app/src/main/java/org/freedu/studentmanagementapp/viewmodels/StudentViewModel.kt
@@ -0,0 +1,40 @@
+package org.freedu.studentmanagementapp.viewmodels
+
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.freedu.studentmanagementapp.models.Student
+import org.freedu.studentmanagementapp.repositories.StudentRepository
+
+class StudentViewModel(private val repository: StudentRepository):ViewModel() {
+    private val _students = MutableLiveData<List<Student>>()
+    val students:LiveData<List<Student>> get() = _students
+
+    fun addStudent(student: Student, imageUri:Uri){
+        repository.addStudent(student, imageUri){
+            if(it){
+                loadStudents()
+            }
+        }
+    }
+
+    private fun loadStudents() {
+        repository.getStudents { _students.value = it }
+    }
+
+    fun updateStudent(student: Student){
+        repository.updateStudent(student){
+            if(it){
+                loadStudents()
+            }
+        }
+    }
+    fun deleteStudent(studentId:String){
+        repository.deleteStudent(studentId){
+            if(it){
+                loadStudents()
+            }
+        }
+    }
+}
